d1dfc84996427fdcbe32cf43c4fb8aab1fd6245a
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2022-06-28 17:54:40 +0300

    Finalize the team memebers section

---
+++ script.js
@@ -0,0 +1,7 @@
+var storyBtn = document.querySelectorAll('.story-btn');
+storyBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("change");
+    btn.nextElementSibling.classList.toggle('change');
+  });
+});
